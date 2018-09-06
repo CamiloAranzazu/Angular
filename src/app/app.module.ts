f8fcf3d66f76d09c299971a42d38c3b74be411d9
CAMILO ARANZAZU PEREZ <CAMILO ARANZUZA PEREZ>
2018-09-06 17:05:10 -0500

    Creacion de proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule
